9699cbebc8b1b1a47d5619615e6c9841c47d8fb7
=Rahul <rahul.kumar2@coop.co.uk>
2021-07-23 21:17:54 +0530

    this is my first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TypeScript
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! we are here");
+        }
+    }
+}
